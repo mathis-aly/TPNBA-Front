c7a9f06aee8e4e2529007cfc0e31fc32c110a56c
Bogoce <o.barret@myskolae.fr>
2026-06-16 12:59:40 +0200

    Début du js

---
+++ main.js
@@ -0,0 +1,7 @@
+const identifiant = document.getElementById('identifiant')
+const nomJoueur = document.getElementById('nomJoueur')
+const imageJoueur = document.getElementById('imageJoueur')
+const question = document.getElementById('question')
+const reponse = document.getElementById('reponse')
+const btnValider = document.getElementById('btn-valider')
+
